0e6b3c4e2beda86cbbde18ec15594a6b9f680029
ratopi <ratopi@abwesend.de>
2013-09-19 14:54:34 +0200

    remove "start"-function

--- src/webserver/webserver.js
+++ src/webserver/webserver.js
@@ -21,75 +21,71 @@ de.ratopi.sketches = de.ratopi.sketches || {};
 
 // --- package definition
 
-de.ratopi.sketches.webserver =
+de.ratopi.sketches.webserver = function( request, response )
 {
-	"start": function( request, response )
+	function fileHandler( err, file )
 	{
-		function fileHandler( err, file )
+		if ( err )
 		{
-			if ( err )
-			{
-				response.writeHead( 500, { "Content-Type": "text/plain" } );
-				response.write( err + "\n" );
-				response.end();
-				return;
-			}
-
-			var headers = {};
-			
-			var contentType = contentTypesByExtension[ path.extname( filename ) ];
-
-			if ( contentType )
-			{
-				headers[ "Content-Type" ] = contentType;
-			}
-
-			response.writeHead( 200, headers );
-			response.write( file, "binary" );
+			response.writeHead( 500, { "Content-Type": "text/plain" } );
+			response.write( err + "\n" );
 			response.end();
-		};
+			return;
+		}
 
-		// ---
+		var headers = {};
 
-		function pathHandler( exists )
+		var contentType = contentTypesByExtension[ path.extname( filename ) ];
+
+		if ( contentType )
 		{
-			if ( ! exists )
-			{
-				response.writeHead( 404, { "Content-Type": "text/plain" } );
-				response.write( "404 Not Found\n" );
-				response.end();
+			headers[ "Content-Type" ] = contentType;
+		}
 
-				return;
-			}
+		response.writeHead( 200, headers );
+		response.write( file, "binary" );
+		response.end();
+	};
 
-			if ( fs.statSync( filename ).isDirectory() )
-			{
-				filename += '/index.html';
-			}
+	// ---
 
-			fs.readFile( filename, "binary",  fileHandler );
-		};
-		
-		// ---
+	function pathHandler( exists )
+	{
+		if ( ! exists )
+		{
+			response.writeHead( 404, { "Content-Type": "text/plain" } );
+			response.write( "404 Not Found\n" );
+			response.end();
 
-		var uri = url.parse( request.url ).pathname;
-		var filename = path.join( process.cwd(), uri );
+			return;
+		}
 
-		var contentTypesByExtension = {
-			'.html': "text/html",
-			'.css':  "text/css",
-			'.js':   "text/javascript"
-		};
+		if ( fs.statSync( filename ).isDirectory() )
+		{
+			filename += '/index.html';
+		}
 
-		fs.exists( filename, pathHandler );
-	}
+		fs.readFile( filename, "binary",  fileHandler );
+	};
+
+	// ---
+
+	var uri = url.parse( request.url ).pathname;
+	var filename = path.join( process.cwd(), uri );
+
+	var contentTypesByExtension = {
+		'.html': "text/html",
+		'.css':  "text/css",
+		'.js':   "text/javascript"
+	};
+
+	fs.exists( filename, pathHandler );
 };
 
 // ---
 
-http.createServer( de.ratopi.sketches.webserver.start ).listen( parseInt( port, 10 ) );
+http.createServer( de.ratopi.sketches.webserver ).listen( parseInt( port, 10 ) );
 
 console.log( "Static file server running at ");
 console.log( "=> http://localhost:" + port );
 console.log( "CTRL + C to shutdown" );
-
